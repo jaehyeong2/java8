313e94db9833eb8ce00c4e6a3171fdb1144ef5d3
jae <wogud1514@naver.com>
2022-03-08 16:10:44 +0900

    셋팅

---
+++ src/main/java/jjfactory/javathe/JavaTheApplication.java
@@ -0,0 +1,13 @@
+package jjfactory.javathe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaTheApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jjfactory/javathe/JavaTheApplicationTests.java
@@ -0,0 +1,13 @@
+package jjfactory.javathe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
